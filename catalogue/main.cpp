3efbf79228632193feaff0caa7b05c0de66b9f93
kryzhanovskiymax <71114365+kryzhanovskiymax@users.noreply.github.com>
2021-07-15 17:44:23 +0300

    Final main

--- catalogue/main.cpp
+++ catalogue/main.cpp
@@ -1,19 +1,9 @@
-//
-//  main.cpp
-//  catalogue
-//
-//  Created by Максим Крыжановский on 11.07.2021.
-//
-
-#include <iostream>
 #include <iostream>
 #include <fstream>
-#include <chrono>
 
-#include "transport_catalogue.hpp"
-#include "json_reader.hpp"
-#include "request_handler.hpp"
-#include "log_duration.hpp"
+#include "transport_catalogue.h"
+#include "json_reader.h"
+#include "request_handler.h"
 
 using namespace std;
 using namespace json;
@@ -23,59 +13,19 @@ using namespace transport_catalogue::request_handler;
 using namespace transport_catalogue::json_reader;
 using namespace transport_catalogue::request_handler::detail;
 
-using Clock = std::chrono::steady_clock;
-
-void RunSystem(std::fstream& ifs, std::fstream& ofs) {
+void RunSystem() {
     JsonReader jr;
     TransportCatalogue tc;
     RequestHandler rh;
-    jr.ReadJson(ifs);
+    jr.ReadJson(std::cin);
     jr.InitializeTransportCatalogue(tc);
     rh.InitializeRequestHandler(jr.GetRequests());
     rh.HandleRequests(tc);
     jr.WriteResponse(rh.GetResponses());
-    std::cout << "Buses: " << tc.GetBusCount() << std::endl;
-    std::cout << "Stops: " << tc.GetStopCount() << std::endl;
-    std::cout << "Requests: " << rh.GetRequestCount() << std::endl;
-    jr.Print(ofs);
+    jr.Print(std::cout);
 }
 
-int main(int argc, const char * argv[]) {
-    
-    std::cout << "Tests started" << std::endl;
-    
-    /*std::fstream test1;
-    test1.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/test1.json");
-    std::fstream test1_response;
-    test1_response.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/test1_response.json");
-    
-    std::fstream test2;
-    test2.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/test2.json");
-    std::fstream test2_response;
-    test2_response.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/test2_response.json");
-    
-    std::fstream test3;
-    test3.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/test3.json");
-    std::fstream test3_response;
-    test3_response.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/test3_response.json");
-    
-    std::fstream test4;
-    test4.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/test4.json");
-    std::fstream test4_response;
-    test4_response.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/test4_response.json");*/
-    
-    std::fstream ifs;
-    ifs.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/city.json");
-    std::fstream ofs;
-    ofs.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/response.json");
-    
-    const Clock::time_point start_time = Clock::now();
-    
-    RunSystem(ifs, ofs);
-    
-    const auto end_time = Clock::now();
-    const auto dur = end_time - start_time;
-    std::cout << duration_cast<std::chrono::milliseconds>(dur).count() << " ms" << std::endl;
-    
+int main() {
+    RunSystem();
     return 0;
 }
