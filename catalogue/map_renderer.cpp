ef1d5920b783592f8aa48b03484297fa655bae2d
Max Kryzhanovskiy <kryzhanovskiymax@mail.ru>
2021-07-25 19:23:29 +0300

    Some updates to MapRenderer

--- catalogue/map_renderer.cpp
+++ catalogue/map_renderer.cpp
@@ -1,46 +1,65 @@
+#include "map_renderer.hpp"
 
-//using namespace transport_catalogue::map_renderer;
+using namespace transport_catalogue;
+using namespace transport_catalogue::detail;
+using namespace transport_catalogue::map_renderer;
 
-/*void MapRenderer::CreateMap(std::vector<std::string, std::pair<bool, std::vector<std::string>>> buses, std::vector<std::pair<std::string, Coordinates>> stops) {
-    
-}
-
-void MapRenderer::CalculateCoefficients(std::vector<Coordinates> coordinates_container) {
-    std::vector<double> latitudes;
+void MapRenderer::CalculateCoefficients(std::vector<transport_catalogue::detail::Coordinates> stops_coordinates) {
     std::vector<double> longitudes;
+    std::vector<double> latitudes;
     
-    for(const auto& coordinate : coordinates_container) {
-        latitudes.push_back(coordinate.lat);
-        longitudes.push_back(coordinate.lng);
+    for(const auto& coordinates : stops_coordinates) {
+        longitudes.push_back(coordinates.lng);
+        latitudes.push_back(coordinates.lat);
     }
     
     min_lat = *std::min(latitudes.begin(), latitudes.end());
-    max_lat = *std::max(latitudes.begin(), latitudes.end());
-    min_lon = *std::min(longitudes.begin(), longitudes.end());
-    max_lon = *std::max(longitudes.begin(), longitudes.end());
-    
+    max_lat = *std::max(longitudes.begin(), longitudes.end());
+    min_lng = *std::min(longitudes.begin(), longitudes.end());
+    max_lng = *std::max(longitudes.begin(), longitudes.end());
     
     height_zoom_coef = (height - 2*padding)/(max_lat - min_lat);
-    width_zoom_coef = (width - 2*padding)/(max_lon - min_lon);
+    width_zoom_coef = (width - 2*padding)/(max_lng-min_lng);
 }
 
-svg::Point MapRenderer::TransformCoordinateToPoint(Coordinates coordinates) {
-    double zoom_coef;
-    
-    if(height_zoom_coef == 0) {
-        zoom_coef = width_zoom_coef;
-    } else if(width_zoom_coef == 0) {
-        zoom_coef = height_zoom_coef;
-    } else {
-        if(height_zoom_coef > width_zoom_coef) {
-            zoom_coef = width_zoom_coef;
-        } else {
-            zoom_coef = height_zoom_coef;
-        }
-    }
-    
-    double x = (coordinates.lng - min_lon)*zoom_coef + padding;
-    double y = (max_lat - coordinates.lat)*zoom_coef + padding;
-    
-    return svg::Point{x, y};
-}*/
+
+
+void MapRenderer::SetWidth(double width_) {
+    width = width_;
+}
+
+void MapRenderer::SetHeight(double height_) {
+    height = height_;
+}
+
+void MapRenderer::SetPadding(double padding_) {
+    padding = padding_;
+}
+
+void MapRenderer::SetLineWidth(double line_width_) {
+    line_width = line_width_;
+}
+
+void MapRenderer::SetStopRadius(double stop_radius_) {
+    stop_radius = stop_radius_;
+}
+
+void MapRenderer::SetBusLabelFontSize(double bus_label_font_size_) {
+    bus_label_font_size = bus_label_font_size_;
+}
+
+void MapRenderer::SetBusLabelOffset(double bus_label_offset_) {
+    bus_label_offset = bus_label_offset_;
+}
+
+void MapRenderer::SetUnderlayerWidth(double underlayer_width_) {
+    underlayer_width = underlayer_width_;
+}
+
+void MapRenderer::SetUnderlayerColor(svg::Color underlayer_color_) {
+    underlayer_color = underlayer_color_;
+}
+
+void MapRenderer::SetColorPalette(std::vector<svg::Color> color_palette_) {
+    color_palette = color_palette_;
+}
--- catalogue/map_renderer.hpp
+++ catalogue/map_renderer.hpp
@@ -1,10 +1,61 @@
 #pragma once
 
-
+#include <map>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
 
 #include "transport_catalogue.hpp"
 #include "geo.h"
 #include "svg_package.hpp"
 
 
+namespace transport_catalogue {
 
+namespace map_renderer {
+
+class MapRenderer {
+public:
+    void CreateMap(std::vector<transport_catalogue::detail::Stop> stops, std::vector<transport_catalogue::detail::Bus> buses);
+    void CalculateCoefficients(std::vector<transport_catalogue::detail::Coordinates> stops_coordinates);
+    
+    void SetWidth(double width_);
+    void SetHeight(double height_);
+    void SetPadding(double padding_);
+    void SetLineWidth(double line_width_);
+    void SetStopRadius(double stop_radius_);
+    void SetBusLabelFontSize(double bus_label_font_size_);
+    void SetBusLabelOffset(double bus_label_offset_);
+    void SetUnderlayerWidth(double underlayer_width_);
+    void SetUnderlayerColor(svg::Color underlayer_color_);
+    void SetColorPalette(std::vector<svg::Color> color_palette_);
+private:
+    std::map<std::string, std::pair<bool, std::vector<std::string>>> bus_to_stops;
+    std::map<std::string, svg::Point> stop_to_position;
+    
+    double width;
+    double height;
+    double padding;
+    double line_width;
+    double stop_radius;
+    double bus_label_font_size;
+    double bus_label_offset;
+    double underlayer_width;
+    svg::Color underlayer_color;
+    std::vector<svg::Color> color_palette;
+    
+    double min_lat;
+    double max_lat;
+    double min_lng;
+    double max_lng;
+    
+    double height_zoom_coef = 0;
+    double width_zoom_coef = 0;
+    
+    svg::Point TranslateCoordinatesToPoint(transport_catalogue::detail::Coordinates coordinates);
+};
+
+}
+
+}
