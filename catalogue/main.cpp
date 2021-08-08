d54cd22f022ee8012a3706a3fa7f300d13ef1748
Max Kryzhanovskiy <kryzhanovskiymax@mail.ru>
2021-08-08 20:44:27 +0300

    latest update

--- catalogue/main.cpp
+++ catalogue/main.cpp
@@ -43,41 +43,21 @@ void RunSystem(std::istream& is, std::ostream& out_base, std::ostream& out_map) 
 }
 
 int main() {
-    
-    
-    //tc.InitializeMapRenderer(mr);
-    LogDuration dur("MapRenderer Test", std::cout);
     clock_t start = clock();
     
-    std::ifstream is;
-    is.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/city.json");
+    std::ifstream base_input;
+    base_input.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/city.json");
     
-    std::ifstream ifs;
-    ifs.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/city_data_base.json");
+    std::ofstream base_output;
+    base_output.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/response.json");
     
-    std::ofstream os;
-    os.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/response.json");
-    
-    std::ofstream ofs;
-    ofs.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/city.svg");
-    
-    JsonReader jr;
-    TransportCatalogue tc;
-    RequestHandler rh;
-    MapRenderer mr;
-    
-    jr.ReadJson(is);
-    jr.InitializeTransportCatalogue(tc);
-    rh.InitializeRequestHandler(jr.GetRequests());
-    rh.HandleRequests(tc);
-    jr.WriteResponse(rh.GetResponses());
-    jr.Print(os);
-    
+    std::ofstream map_output;
+    map_output.open("/Users/makskryzhanovskiy/Desktop/Projects/do IT/Tests/city.svg");
     
     
     std::cout << "System Run started" << std::endl;
     {
-        RunSystem(is, os, ofs);
+        RunSystem(base_input, base_output, map_output);
     }
     std::cout << "System Run finished" << std::endl;
     
